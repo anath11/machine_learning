843eb45039f09e78035add752a7bd176c2b97705
Ashok Nath <nathashok87@gmail.com>
2020-08-23 19:52:01 +1000

    Changing the py file

--- ml.py
+++ ml.py
@@ -1,15 +1,30 @@
-# import pandas as pd
-
-# data_frame = pd.read_csv('data.csv')
-
-# data_frame.shape
-
-# # data_frame.head
-
 import pandas as pd
+data_frame = pd.read_csv('data.csv')
+data_frame.shape
 
-df = pd.read_csv('data.csv')
-df.shape
-df.head
-df.tail
-print(df.mean)
+df1 = pd.DataFrame(data_frame, columns=['Name', 'Wage', 'Value'])
+
+
+def value_to_float(x):
+    if type(x) == float or type(x) == int:
+        return x
+    if 'K' in x:
+        if len(x) > 1:
+            return float(x.replace('K', '')) * 1000
+        return 1000.0
+    if 'M' in x:
+        if len(x) > 1:
+            return float(x.replace('M', '')) * 1000000
+        return 1000000.0
+    if 'B' in x:
+        return float(x.replace('B', '')) * 1000000000
+    return 0.0
+
+
+wage = df1['Wage'].replace('[\€]', '', regex=True).apply(value_to_float)
+value = df1['Value'].replace('[\€]', '', regex=True).apply(value_to_float)
+
+df1['Wage'] = wage
+df1['Value'] = value
+df1['difference'] = df1['Value']-df1['Wage']
+df1
