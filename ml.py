53e619f5bf84eff9e8b02e23fa6dd706dc8c11d8
Ashok Nath <nathashok87@gmail.com>
2020-08-23 19:23:26 +1000

    testing pandas inbuilt functions

--- ml.py
+++ ml.py
@@ -1,4 +1,15 @@
+# import pandas as pd
+
+# data_frame = pd.read_csv('data.csv')
+
+# data_frame.shape
+
+# # data_frame.head
+
 import pandas as pd
 
-data_frame = pd.read_csv('data.csv')
-print(data_frame)
+df = pd.read_csv('data.csv')
+df.shape
+df.head
+df.tail
+print(df.mean)
